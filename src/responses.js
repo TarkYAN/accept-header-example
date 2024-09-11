629b2588ed7eb57a31dbd1420cc14a8c406425ce
LordOden <butter2002at@gmail.com>
2024-09-11 14:48:07 -0400

    Sept. 11 class content added

--- src/responses.js
+++ src/responses.js
@@ -3,13 +3,35 @@ const fs = require('fs'); // pull in the file system module
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
 const respond = (request, response, content, type) => {
-  response.writeHead(200, { 'Content-Type': type });
+  response.writeHead(200, { 
+    'Content-Type': type,
+    'Content-Length': Buffer.byteLength(content, 'utf8'), 
+  });
   response.write(content);
   response.end();
 };
 
 const getIndex = (request, response) => {
   respond(request, response, index, 'text/html');
+};
+
+const getCats = (request, response) => {
+  const cat = {
+    name: 'Whiskers',
+    age: 2
+  };
+
+  if(request.acceptedTypes[0] === 'text/xml'){
+    let xmlString = '<response>';
+    xmlString += `<name>${cat.name}</name>`;
+    xmlString += `<age>${cat.age}</age>`;
+    xmlString += '</response>'
+
+    return respond(request, response, xmlString, 'text/xml');
+  }
+
+  const catString = JSON.stringify(cat);
+  return respond(request, response, catString, 'application/json');
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -4,11 +4,30 @@ const responseHandler = require('./responses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-
+  '/': responseHandler.getIndex,
+  '/cats': responseHandler.getCats,
+  index: responseHandler.getIndex
 };
 
 const onRequest = (request, response) => {
+  const protocol = request.connection.encrypted ? 'https' : 'http';
+  const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);  
+  request.acceptedTypes = request.headers.accept.split(',');
 
+  // const handler = urlStruct[parsedUrl.pathname];
+  // if(handler){
+  //   handler(request, response);
+  // }
+  // else{
+  //   urlStruct.getIndex(request, response);
+  // }
+
+  if(urlStruct[parsedUrl.pathname]){
+    urlStruct[parsedUrl.pathname](request, response);
+  }
+  else{
+    urlStruct.index(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
